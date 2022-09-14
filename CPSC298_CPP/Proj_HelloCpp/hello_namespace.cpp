fc8957bc7087b35789bb847843a17da5e399f067
cjoo03 <cjoo@chapman.edu>
2022-09-14 03:17:17 +0000

    Proj_HelloCpp hello with namespace

---
+++ CPSC298_CPP/Proj_HelloCpp/hello_namespace.cpp
@@ -0,0 +1,11 @@
+// @file hello_namespace.cpp
+// @brief Outputs Hello C++! an a namespace.
+// @author Chris Joo
+
+#include <iostream>
+using namespace std;
+int main()
+{
+  cout << "Hello C++!" << endl;
+  return 0;
+}
