bbf0def15bd1a09bd1f26f4218d8c37e68532299
cjoo03 <cjoo@chapman.edu>
2022-09-14 03:12:34 +0000

    Proj_HelloCpp first hello program

---
+++ CPSC298_CPP/Proj_HelloCpp/hello.cpp
@@ -0,0 +1,10 @@
+// @file hello.cpp
+// @brief C++ program that prints Hello C++.
+// @author Chris Joo
+
+#include <iostream>
+int main()
+{
+  std::cout << "Hello C++!" << std::endl;
+  return 0;
+}
